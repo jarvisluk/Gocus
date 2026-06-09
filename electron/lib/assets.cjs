b172eeccbf496fc8e61ee04484535ded9ad58a61
Jarvis Luk <lu_junrong@outlook.com>
2026-06-10 02:26:01 +0800

    style: refresh Git Peek app icons

--- electron/lib/assets.cjs
+++ electron/lib/assets.cjs
@@ -12,7 +12,23 @@ function createAssetLoader({ nativeImage, resourcesPath, electronDir }) {
   }
 
   function loadImageAsset(fileName, { template = false } = {}) {
-    const image = nativeImage.createFromPath(resolveAssetPath(fileName));
+    const assetPath = resolveAssetPath(fileName);
+    if (!fs.existsSync(assetPath)) {
+      const missingImage = nativeImage.createFromPath(assetPath);
+      if (template) missingImage.setTemplateImage(true);
+      return missingImage;
+    }
+
+    const image = nativeImage.createFromBuffer(fs.readFileSync(assetPath));
+
+    const extension = path.extname(assetPath);
+    const basePath = assetPath.slice(0, -extension.length);
+    const highResolutionPath = `${basePath}@2x${extension}`;
+    if (fs.existsSync(highResolutionPath)) {
+      const dataUrl = `data:image/png;base64,${fs.readFileSync(highResolutionPath).toString("base64")}`;
+      image.addRepresentation({ scaleFactor: 2, dataURL: dataUrl });
+    }
+
     if (template) image.setTemplateImage(true);
     return image;
   }
